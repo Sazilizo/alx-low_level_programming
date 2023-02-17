53f43423fd780a23e39f8074dc7ee367cb097051
Sazilizo <lizosazi@gmail.com>
2023-02-17 09:09:06 -0800

    fix to 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,11 @@
 /**
  * main - prints "Programming is like building a multilingual puzzle followed by a new line" 
  *
- * Return - Always 0 (success)
+ * Return: Always 0
 */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
