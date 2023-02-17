13ef8147396072ce31f91e578f30ea5819bcabb9
Sazilizo <lizosazi@gmail.com>
2023-02-17 07:04:26 -0800

    bett-style fix -writing a programthat prints to screen using puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * this prints aline using put() onto the screen
+ *
+ * returns 0
+ */
+
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
