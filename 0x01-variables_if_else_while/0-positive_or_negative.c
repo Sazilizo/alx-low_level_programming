9b64d52019c994ae0c8202e6dbd883679442a681
Sazilizo <lizosazi@gmail.com>
2023-02-17 14:43:32 -0800

    readme file for variables-if-else-while

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <time.h>
+/**
+ * main -a program that will assign a number to n and print "negative", "positive" or "zero" depending on number
+ *
+ * If : number is greater than 0 print positive
+ * Else if : Number is less than 0 print negative
+ * else : print 0
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else{
+		printf("%d is zero\n", n);
+	}
+	return (0);
+}
