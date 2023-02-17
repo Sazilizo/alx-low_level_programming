b1b25a7d6ccfcaa466633ae6e0efaaadde7fd8e7
Sazilizo <lizosazi@gmail.com>
2023-02-17 07:19:52 -0800

    added description to function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 /**
- * this prints aline using put() onto the screen
+ * main -Entry point
  *
- * returns 0
+ * puts - prints "Programming is like building a multilingual puzzle followed by a new line 
+ *
+ * Return : returns (0)
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
