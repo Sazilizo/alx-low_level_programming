f09304e4ca1cd714f0412c265b848d0172638609
Sazilizo <lizosazi@gmail.com>
2023-02-22 15:49:13 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _abs - prints the absolute value of num
+ *
+ * Return : always 0
+ */
+int _abs(int n)
+{
+printf("%d", abs(n));
+return (0);
+}
