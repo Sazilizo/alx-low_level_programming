30ccf146986fac6791b6fd153beb9b6b9d57d31b
Sazilizo <lizosazi@gmail.com>
2023-02-23 14:22:50 -0800

    0x03-debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests a function which prints pos or neg
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+}
