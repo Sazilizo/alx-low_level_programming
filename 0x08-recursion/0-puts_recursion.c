28d8354858008ccf7d1a8bbbac4ee299de1faec9
Sazilizo <lizosazi@gmail.com>
2023-06-08 08:17:49 -0700

    1-print_rev_recusion.c and 2-strlen_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
 #include "main.h"
 
-
 /**
- * _puts_recursion - prints single characters to screen
- * @s: feeder string
- * @Return: void
+ * _puts_recursion - Entry point
+ * @s: String feeder
  */
 
 void _puts_recursion(char *s)
 {
-	if (*s == '\0') return;
-	else _putchar(*s++);
+	if(*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar(*s++);
+	}
 	_puts_recursion(s++);
 }
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - counts the number of characters in string
+ * @s: string feeder
+ * @Return: length of the string
+ */
+
+int _strlen_recursion(char *s)
+{
+
+	if( *s == '\0')
+		return (0);
+	else
+	{
+		return 1 + _strlen_recursion( s + 1);
+
+	}
+	
+}
