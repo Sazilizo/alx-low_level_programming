073bc81a9b3665868dd789605074d978899fbabf
Sazilizo <lizosazi@gmail.com>
2023-06-08 10:49:27 -0700

    4-pow_recursion.c betty and documentation

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -5,14 +5,14 @@
  * _pow_recursion -  raises x to the power of y  and return x
  * @x: base number
  * @y: power number
- * @return: x to the power of y
+ * Return: x to the power of y
  */
 
 int _pow_recursion(int x, int y)
 {
-	if ( y == 0)
+	if (y == 0)
 		return (1);
-	if (y < 0 )
+	if (y < 0)
 		return (-1);
 	else
 		return (x * _pow_recursion(x, y - 1));
