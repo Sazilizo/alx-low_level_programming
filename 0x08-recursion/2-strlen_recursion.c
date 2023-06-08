5df984361d77123fcfc900e89633597b10f0dfbc
Sazilizo <lizosazi@gmail.com>
2023-06-08 10:45:03 -0700

    4-pow_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,13 +4,13 @@
 /**
  * _strlen_recursion - counts the number of characters in string
  * @s: string feeder
- * @return: length of the string
+ * Return: length of the string
  */
 
 int _strlen_recursion(char *s)
 {
 	if (*s != '\0')
-		return 1 + _strlen_recursion( s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	return (0);
 
 }
