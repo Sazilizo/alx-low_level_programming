0ebf5595ebeb560679baf36f44d85c1791a66057
Sazilizo <lizosazi@gmail.com>
2023-06-09 03:27:02 -0700

    addition of 1-args.c file

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -Entry poitn
+ * @argc: number of args passed
+ * @argv: array of args passed
+ * Return: always (0) success
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
