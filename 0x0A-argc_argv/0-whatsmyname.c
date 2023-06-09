5593574155edd7309f55b990d0ed2a690ac57e88
Sazilizo <lizosazi@gmail.com>
2023-06-09 03:21:12 -0700

    readme file for 0x0A-argv and 0-whatsmyname.c file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/** main - entry point
+ * @argc: number o commands listed
+ * @argv: array of passed arguments/commands
+ * Retuen:always (0) success
+ */
+
+int main(int argc __attribute__((unused)) , char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+command line arguments, argc and argv, argc holds the number of arguments passed while argv[] is the array of commands
