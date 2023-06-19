bdd30e4eebf003a4076b6180560ad15243f1ce84
Sazilizo <lizosazi@gmail.com>
2023-06-19 07:49:10 -0700

    correction to defining dog_t as a datatype of struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+};
 
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner)
