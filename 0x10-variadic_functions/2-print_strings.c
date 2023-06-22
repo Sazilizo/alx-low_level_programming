b469c1fda76a6b6f6545fe0584f1169cab417585
Sazilizo <lizosazi@gmail.com>
2023-06-22 14:16:12 -0700

    check for null string in 2-print_strinc.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,6 +20,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		printf("%s", string);
 
+		if (string == NULL)
+			printf("(nil)\n");
 		if (i < n - 1 && separator != NULL)
 			printf("%c ", *separator);
 	}
