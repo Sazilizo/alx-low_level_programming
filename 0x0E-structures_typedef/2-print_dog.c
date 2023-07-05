82bf8de4394fe37078408570234e687313c3cd97
Sazilizo <lizosazi@gmail.com>
2023-07-05 14:11:55 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,35 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
-#include <math.h>
+
 /**
  * print_dog -prints out a dog struct
- * @struct d:dog structutre
+ * @d:dog structutre
  */
 
 void print_dog(struct dog *d)
 {
-
-	struct dog *new_dog;
-
-	new_dog = malloc(sizeof(d));
-
-	if (new_dog == NULL)
-		exit (98);
-	new_dog->name = malloc(sizeof(d->name));
-	new_dog->owner = malloc(sizeof(d->owner));
-
-	if (new_dog->name == NULL)
+	if (d == NULL)
+		exit(98);
+	if (d->name == NULL)
 		printf("Name: (nil)\n");
-	if (new_dog->owner == NULL)
+	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 
-	new_dog->name = d->name;
-	new_dog->age = d->age;
-	new_dog->owner = d->owner;
-
-
-	printf("Name: %s\n",new_dog->name);
-	printf("Age: %f\n", new_dog->age);
-	printf("Owner: %s\n", new_dog->owner);
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 }
